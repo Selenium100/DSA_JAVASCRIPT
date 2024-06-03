3951c0d9334dfb6db35621dd438139f8b1ff54db
Nitya <nityaranjan190592@gmail.com>
2024-06-03 11:54:48 +0530

    test: bubble sort is added

---
+++ bubble_sort.js
@@ -0,0 +1,18 @@
+function bubblesort(arr){
+let n = arr.length;
+
+for(let i=0;i<n;i++){
+    for(let j=i+1;j<n;j++){
+        if(arr[i]>arr[j]){
+            let temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+return arr;
+}
+
+let arr = [5,2,3,8,1,0];
+let sortedArr = bubblesort(arr);
+console.log(sortedArr);
